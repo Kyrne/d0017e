f1b2d80d97c429f07f72e940cee8488e9b473bac
Kyrne <johnnylam0914@gmail.com>
2020-10-07 17:52:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -44,58 +44,58 @@ void substring(char source[], int start, int count, char result[]){
 
 #if 0 //Task 1 b)
 int findString(char[], char[]);
-void getLine(char buffer[]);
 
 void main(){
-    char source[]="", word[]="", temp[]="";
+    char source[99]="", word[99]="";
     printf("Input a sentence:");
-    getLine(temp);
-    printf("\n%s",source);
+    scanf ("%[^\n]%*c", source);
+
     printf("\nInput the word you want to find:");
-    getLine(temp);
-    printf("\n%s",word);
-    findString(source, word);
-    printf("\n%s",source);
-    printf("\n%s",word);
+    scanf ("%[^\n]%*c", word);
+
+    printf("%i",findString(source, word));
+
 }
 
-void getLine(char buffer[]){
-    char character;
-    int i = 0;
-    do{
-        character = getchar();
-        buffer[i] = character;
-        i++;
-    }while(character !='\n');
-    buffer[i-1]='\0';
-}
 
 int findString(char source[], char word[]){
-    int lengthSource = strlen(source);
-    int lengthWord = strlen(word);
-    int n = 0;
+    int sourceLength = strlen(source);
+    int wordLength = strlen(word);
+    int temp = 0, counter = 0;
 
-    for(int i = 0; i <= lengthWord; i++){
-        while(word[i]!=source[n]){
-            n++;
-        }
-        if(n==lengthWord){
-            return -1;
+    for (int n = 0; n<wordLength; n++) {
+        for (int i = temp; i <= sourceLength - wordLength + 1; i++) {
+            if (source[i] == word[n]) {
+                temp = i;
+                counter++;
+                break;
+            }
+            else{
+                temp = 0;
+            }
         }
     }
-
+    if(counter == wordLength)
+        return temp-wordLength+1;
+    else
+        return -1;
 }
+
 #endif
 
 #if 1 //Task 2 a)
 void insertEntry();
 
+typedef struct{
+    int value;
+}entry;
+
 void main(){
-
-
+    int insert = 1337;
+    insertEntry(insert);
 }
 
-void insertEntry(){
+void insertEntry(entry value){
 
 }
 
