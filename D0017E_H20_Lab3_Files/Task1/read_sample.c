cf04f26378969c6ec4cc2a4e34448d6bfa849c6e
Kyrne <johnnylam0914@gmail.com>
2020-10-14 15:46:06 +0200

    uppg 12 ya yeet

--- D0017E_H20_Lab3_Files/Task1/read_sample.c
+++ D0017E_H20_Lab3_Files/Task1/read_sample.c
@@ -5,7 +5,7 @@ void main(){
     int width=32, height=32, *pWidth=&width, *pHeight=&height;
     PIXEL_RGB24 *pixelBuffer, **pBuffer=&pixelBuffer;
 
-    tga_read("C:\\Users\\johnn\\Documents\\GitHub\\d0017e\\D0017E_H20_Lab3_Files\\Task1\\sample.tga", pWidth, pHeight, pBuffer);
-    tga_write("C:\\Users\\johnn\\Documents\\GitHub\\d0017e\\D0017E_H20_Lab3_Files\\Task1\\sample-24bit.tga", 64, 64, pixelBuffer, 24);
+    tga_read("sample.tga", pWidth, pHeight, pBuffer);
+    tga_write("sample-24bit.tga", 64, 64, pixelBuffer, 24);
 
 }
