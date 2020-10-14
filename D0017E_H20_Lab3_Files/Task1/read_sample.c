2fdac21d204da705640aa8c499f8e9a1ed159c7a
Kyrne <johnnylam0914@gmail.com>
2020-10-14 14:29:35 +0200

    write func

--- D0017E_H20_Lab3_Files/Task1/read_sample.c
+++ D0017E_H20_Lab3_Files/Task1/read_sample.c
@@ -2,9 +2,10 @@
 #include "tga.h"
 
 void main(){
-    int width=64, height=64, *pWidth=&width, *pHeight=&height;
+    int width=32, height=32, *pWidth=&width, *pHeight=&height;
     PIXEL_RGB24 *pixelBuffer, **pBuffer=&pixelBuffer;
 
     tga_read("C:\\Users\\johnn\\Documents\\GitHub\\d0017e\\D0017E_H20_Lab3_Files\\Task1\\sample.tga", pWidth, pHeight, pBuffer);
+    tga_write("C:\\Users\\johnn\\Documents\\GitHub\\d0017e\\D0017E_H20_Lab3_Files\\Task1\\sample-24bit.tga", 64, 64, pixelBuffer, 24);
 
 }
